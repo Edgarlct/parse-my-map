719a6cb9a7d9bae5559c2a177a8a5dd50d38bdba
Edgarlct <edgar.lecomte@icloud.com>
2023-10-18 14:26:45 +0200

    :construction: Add leaflet to web component

--- src/map-preview.ts
+++ src/map-preview.ts
@@ -1,5 +1,6 @@
 import { LitElement, html, css } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
+import L from "leaflet";
 
 @customElement('leaflet-map')
 export class LeafletMap extends LitElement {
@@ -14,11 +15,26 @@ export class LeafletMap extends LitElement {
             display: block;
         }
         #map {
-            background: red;
+            background: lightgray;
             width: var(--map-width, 600px);
             height: var(--map-height, 400px);
         }
     `;
+
+    @property({ type: Object})
+    center = { lat: 51.505, lng: -0.09 };
+    @property({ type: Number})
+    zoom = 13;
+
+    private map?: L.Map;
+
+    firstUpdated() {
+        this.map = L.map(this.shadowRoot!.getElementById('map')!).setView([this.center.lat, this.center.lng], this.zoom);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '© OpenStreetMap contributors'
+        }).addTo(this.map);
+    }
 
     updated(changedProperties: Map<string | number | symbol, unknown>): void {
         super.updated(changedProperties);
@@ -30,6 +46,7 @@ export class LeafletMap extends LitElement {
 
     render() {
         return html`
+            <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
             <div id="map"></div>
             <button @click="${this.download}">Download ${this.type} from ${this.path}</button>
         `;
